01ba6de9a9e01624c3c5019eef56ad9eff7b49c5
bushey <cbushey@wisc.edu>
2018-05-02 21:22:37 -0500

    Finished testbench

--- snn_core_tb.sv
+++ snn_core_tb.sv
@@ -10,7 +10,7 @@ module snn_core_tb();
   reg clk, rst_n;
   reg start;
   wire q_input[9:0];
-  logic [9:0] addr_input_unit[9:0];
+  wire [9:0] addr;
   wire [3:0] digit;
   wire done;
   
@@ -19,32 +19,125 @@ module snn_core_tb();
   reg [639:0] err_str;
   reg [1023:0] file_line;
   string filename;
-  genvar i;
   
+  wire [9:0] addr0, addr1, addr2, addr3, addr4, addr5, addr6, addr7, addr8, addr9;
+
   reg data;
   reg we;
   integer addr_select;
   
+  assign addr = addr_select == 0 ? addr0 :
+                addr_select == 1 ? addr1 :
+                addr_select == 2 ? addr2 :
+                addr_select == 3 ? addr3 :
+                addr_select == 4 ? addr4 :
+                addr_select == 5 ? addr5 :
+                addr_select == 6 ? addr6 :
+                addr_select == 7 ? addr7 :
+                addr_select == 8 ? addr8 :
+                                   addr9 ;
+
+
+
   snn_core iDUT(.clk(clk)
               , .rst_n(rst_n)
               , .q_input(q_input[addr_select])
               , .start(start)
               , .done(done)
-              , .addr_input_unit(addr_input_unit[addr_select])
+              , .addr_input_unit(addr)
               , .digit(digit));
-  
-  generate 
-    for(i = 0; i < 10; i = i+1) begin: INPUT_RAM_INIT
-      ram #(.DATA_WIDTH(1)
-          , .ADDR_WIDTH(10)
-          , .INIT_FILE($sformatf("input_samples/ram_input_contents_sample_%d.txt", i)))
-          number_input(.data(data)
-                     , .addr(addr_input_unit[i])
-                     , .we(we)
-                     , .clk(clk)
-                     , .q(q_input[i]));
-    end
-  endgenerate
+
+
+    ram #(.DATA_WIDTH(1)
+        , .ADDR_WIDTH(10)
+        , .INIT_FILE("input_samples/ram_input_contents_sample_0.txt"))
+        number_input0(.data(data)
+                    , .addr(addr0)
+                    , .we(we)
+                    , .clk(clk)
+                    , .q(q_input[0]));
+                    
+    ram #(.DATA_WIDTH(1)
+        , .ADDR_WIDTH(10)
+        , .INIT_FILE("input_samples/ram_input_contents_sample_1.txt"))
+        number_input1(.data(data)
+                    , .addr(addr1)
+                    , .we(we)
+                    , .clk(clk)
+                    , .q(q_input[1]));
+
+    ram #(.DATA_WIDTH(1)
+        , .ADDR_WIDTH(10)
+        , .INIT_FILE("input_samples/ram_input_contents_sample_2.txt"))
+        number_input2(.data(data)
+                    , .addr(addr2)
+                    , .we(we)
+                    , .clk(clk)
+                    , .q(q_input[2]));
+   
+    ram #(.DATA_WIDTH(1)
+        , .ADDR_WIDTH(10 )
+        , .INIT_FILE("input_samples/ram_input_contents_sample_3.txt"))
+        number_input3(.data(data)
+                    , .addr(addr3)
+                    , .we(we)
+                    , .clk(clk)
+                    , .q(q_input[3]));
+   
+    ram #(.DATA_WIDTH(1)
+        , .ADDR_WIDTH(10 )
+        , .INIT_FILE("input_samples/ram_input_contents_sample_4.txt"))
+        number_input4(.data(data)
+                    , .addr(addr4)
+                    , .we(we)
+                    , .clk(clk)
+                    , .q(q_input[4]));
+   
+    ram #(.DATA_WIDTH(1)
+        , .ADDR_WIDTH(10)
+        , .INIT_FILE("input_samples/ram_input_contents_sample_5.txt"))
+        number_input5(.data(data)
+                    , .addr(addr5)
+                    , .we(we)
+                    , .clk(clk)
+                    , .q (q_input[5]));
+
+    ram #(.DATA_WIDTH(1)
+        , .ADDR_WIDTH(10 )
+        , .INIT_FILE("input_samples/ram_input_contents_sample_6.txt"))
+        number_input6(.data(data)
+                    , .addr(addr6)
+                    , .we(we)
+                    , .clk(clk)
+                    , .q(q_input[6]));
+
+    ram #(.DATA_WIDTH(1)
+        , .ADDR_WIDTH(10)
+        , .INIT_FILE("input_samples/ram_input_contents_sample_7.txt"))
+        number_input7(.data(data)
+                    , .addr(addr7)
+                    , .we(we)
+                    , .clk(clk)
+                    , .q (q_input[7]));
+
+    ram #(.DATA_WIDTH(1)
+        , .ADDR_WIDTH(10)
+        , .INIT_FILE("input_samples/ram_input_contents_sample_8.txt"))
+        number_input8(.data(data)
+                    , .addr(addr8)
+                    , .we(we)
+                    , .clk(clk)
+                    , .q(q_input[8]));
+
+    ram #(.DATA_WIDTH(1)
+        , .ADDR_WIDTH(10 )
+        , .INIT_FILE("input_samples/ram_input_contents_sample_9.txt"))
+        number_input9(.data(data)
+                    , .addr(addr9)
+                    , .we(we)
+                    , .clk(clk)
+                    , .q(q_input[9]));
+    
 
   // Task to properly set up tests
   task initialize;
@@ -67,12 +160,22 @@ module snn_core_tb();
       @(posedge clk);
       start = 1'b0;
       
-      @(posedge done);
-      if(digit == addr_select)
-        $display("Passed test\n");
-      else
-        $display("Failed Test\n");
-        
+      fork 
+        begin: NORMAL_CASE
+          @(posedge done);
+          if(digit == addr_select)
+            $display("Passed test #%d.\n", addr_select);
+          else
+            $display("Failed Test #%d: output %d.\n", addr_select, digit);
+          disable TIMEOUT;
+        end
+
+        begin: TIMEOUT
+          repeat(60000) @(posedge clk);
+          $display("FAILED :: Test timeout for test #%d.", addr_select);
+          $stop;
+        end
+      join
     end
     $stop;
   end 
